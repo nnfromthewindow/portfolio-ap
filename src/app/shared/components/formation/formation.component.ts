686c1a1b6e5034b4ceae46dc81c45ae97628da18
Nico <nuccelli@hotmail.com>
2022-11-08 20:20:52 -0300

    Trabajando hover cardblob

--- src/app/shared/components/formation/formation.component.ts
+++ src/app/shared/components/formation/formation.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import anime from 'animejs'
+import anime from 'animejs';
 
 @Component({
   selector: 'app-formation',
   templateUrl: './formation.component.html',
   styleUrls: ['./formation.component.css'],
 })
-
-
 export class FormationComponent implements OnInit {
   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
   from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
@@ -17,21 +15,49 @@ export class FormationComponent implements OnInit {
 
   formationSubTitle = `Croupier`;
 
+  enter() {
+    console.log('Enter');
+  }
+  out() {
+    console.log('Out');
+  }
 
   constructor() {}
 
   ngOnInit() {
     anime({
-      targets: '#demo-svg path',
-  d: [
-    {value: 'M805.5,597Q767,694,657.5,671Q548,648,474,727.5Q400,807,288,771Q176,735,101,617.5Q26,500,105,385Q184,270,298.5,251Q413,232,496,244.5Q579,257,629,313.5Q679,370,761.5,435Q844,500,805.5,597Z'},
-    {value: 'M805.5,897Q768,694,657.6,671Q549,648,474,727.5Q400,8007,2808,771Q176,735,1001,617.5Q26,500,105,385Q184,2700,2908.5,251Q413,232,4096,244.5Q579,257,629,313.5Q679,370,761.5,435Q844,5000,805.5,597Z'}
-  ],
-  easing:'easeOutQuad',
-  duration: 2500,
-  loop:true,
-  direction:'alternate'
+      targets: '#blob-item path',
+      d: [
+        {
+          value:
+            'M35.8,-43.1C44.7,-27,48.8,-13.5,50.9,2.1C52.9,17.6,52.9,35.3,44.1,43.9C35.3,52.5,17.6,52.1,1.8,50.3C-14.1,48.5,-28.1,45.3,-44.8,36.7C-61.4,28.1,-80.7,14.1,-81.2,-0.5C-81.6,-15,-63.3,-30,-46.6,-46.1C-30,-62.2,-15,-79.5,-0.7,-78.8C13.5,-78,27,-59.3,35.8,-43.1Z',
+        },
+        {
+          value:
+            'M42.2,-41.8C55.6,-28.8,67.8,-14.4,66,-1.8C64.2,10.8,48.3,21.6,35,34C21.6,46.5,10.8,60.5,-2.9,63.4C-16.5,66.3,-33.1,57.9,-42.1,45.5C-51.1,33.1,-52.6,16.5,-49.2,3.4C-45.8,-9.7,-37.5,-19.5,-28.5,-32.4C-19.5,-45.4,-9.7,-61.6,2.3,-64C14.4,-66.3,28.8,-54.8,42.2,-41.8Z',
+        },
+      ],
+      easing: 'easeOutQuad',
+      duration: 2500,
+      loop: true,
+      direction: 'alternate',
     });
-
+    anime({
+      targets: '#blob-img path',
+      d: [
+        {
+          value:
+            'M47.9,-45.1C63.9,-31.9,80,-16,77.5,-2.5C75,11,54.1,22.1,38.1,31.1C22.1,40.1,11,47.1,-4.2,51.3C-19.5,55.6,-39,57,-47.9,48C-56.8,39,-55.2,19.5,-56.8,-1.6C-58.5,-22.8,-63.4,-45.5,-54.5,-58.7C-45.5,-71.8,-22.8,-75.3,-3.4,-71.9C16,-68.5,31.9,-58.2,47.9,-45.1Z',
+        },
+        {
+          value:
+            'M54,-47C68.1,-39.8,76.5,-19.9,70.6,-5.9C64.7,8.1,44.5,16.2,30.3,32.3C16.2,48.5,8.1,72.6,-3.9,76.5C-15.8,80.4,-31.6,63.9,-43.4,47.8C-55.2,31.6,-62.9,15.8,-65.5,-2.6C-68.1,-21,-65.6,-42,-53.8,-49.2C-42,-56.4,-21,-49.7,-0.6,-49.1C19.9,-48.6,39.8,-54.1,54,-47Z',
+        },
+      ],
+      easing: 'easeOutQuad',
+      duration: 2500,
+      loop: true,
+      direction: 'alternate',
+    });
   }
 }
